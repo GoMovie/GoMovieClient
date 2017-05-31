dd68ba1318d129d3df7a680d3c5b19f35154b92a
hhk <codeforsomeone@gmail.com>
2017-05-31 10:45:35 +0800

    改善排行榜

--- src/components/home/home-rank.vue
+++ src/components/home/home-rank.vue
@@ -2,10 +2,10 @@
 <template>
   <div class="home-rank">
     <div>
-      <div class="home-rank__title">排行榜</div>
+      <div class="home-rank__title">今日票房排行榜</div>
       <el-row class="home-rank__item"
-        v-for="item in rankInfo" :key="item.rank">
-        <el-col class="home-rank__rank" :span="2">{{ item.rank }}</el-col>
+        v-for="(item, index) in rankInfo" :key="index">
+        <el-col class="home-rank__rank" :span="2">{{ index + 1 }}</el-col>
         <el-col class="home-rank__name" :span="14">{{ item.name }}</el-col>
         <el-col class="home-rank__hint" :span="8">{{ item.hint }}</el-col>
       </el-row>
@@ -19,13 +19,35 @@ export default {
   data () {
     return {
       rankInfo: [{
-        rank: 1,
-        name: '加勒比海盗',
-        hint: '4096人想看'
+        name: '加勒比海盗5：死无对证',
+        hint: '754.00万'
       }, {
-        rank: 2,
-        name: '摔跤吧，爸爸',
-        hint: '2048人想看'
+        name: '摔跤吧！爸爸',
+        hint: '198.94万'
+      }, {
+        name: '神奇女侠',
+        hint: '173.81万'
+      }, {
+        name: '哆啦A梦：大雄的南极冰冰凉大冒险',
+        hint: '160.36万'
+      }, {
+        name: '荡寇风云',
+        hint: '59.01万'
+      }, {
+        name: '“吃吃”的爱',
+        hint: '41.36万'
+      }, {
+        name: '临时演员',
+        hint: '19.63'
+      }, {
+        name: '夏天19岁的肖像',
+        hint: '17.31万'
+      }, {
+        name: '异星觉醒',
+        hint: '17.12万'
+      }, {
+        name: '三只小猪2',
+        hint: '8.92万'
       }]
     }
   }
@@ -46,25 +68,33 @@ export default {
 
 .home-rank__item {
   text-align: left;
-  font-size: 18px;
-  padding-top: 7px;
-  padding-bottom: 7px;
+  
+  line-height: 18px;
+  padding: 12px 0;
   cursor: pointer;
 }
 
 .home-rank__item:hover {
-  background-color: rgba(238, 241, 246, 1.00);
+  background-color: rgb(238, 241, 246);
 }
 
 .home-rank__rank {
+  font-size: 18px;
   font-weight: bold;
+  opacity: 0.5;
 }
 
 .home-rank__name {
-
+  font-size: 16px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  max-width: 190px;
 }
 
 .home-rank__hint {
-  color: #CCCCCC;
+  text-align: right;
+  font-size: 12px;
+  color: #ff0000;
 }
 </style>
