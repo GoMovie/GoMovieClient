859f66be6a1cbe2079ce26c206afe14d5d045340
hhk <codeforsomeone@gmail.com>
2017-05-31 23:21:12 +0800

    完成选电影院界面

---
+++ src/components/cinema-info/cinema-item.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="cinema-item">
+    <el-row>
+      <el-col :span="18" class="cinema-item__info">
+        <div class="cinema-item__row">
+          <span class="cinema-item__name">{{cinema.name}}</span>
+          <el-rate v-model="cinema.score" disabled show-text text-color="#ff9900" text-template="{value}" class="cinema-item__score"></el-rate>
+        </div>
+        <div class="cinema-item__row">地址：{{cinema.location}}</div>
+        <div class="cinema-item__row">影讯：<span v-for="time in cinema.times">{{time}} | </span></div>
+      </el-col>
+      <el-col :span="2" class="cinema-item__price-col">
+        <span class="cinema-item__price">¥{{cinema.price}}起</span>
+      </el-col>
+      <el-col :span="4" class="cinema-item__button-col">
+        <el-button type="primary" @click="goSeatInfo">选座购票</el-button>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'cinema-item',
+  props: ['cinema'],
+  data () {
+    return {
+    }
+  },
+  methods: {
+    goSeatInfo: function () {
+      this.$router.push('/seat-info')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.cinema-item {
+  height: 88px;
+  margin: 0 20px 20px;
+  padding: 10px 20px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  font-size: 14px;
+}
+.cinema-item__info {
+  text-align: left;
+}
+.cinema-item__row {
+  padding: 5px;
+}
+.cinema-item__name {
+  font-size: 18px;
+  line-height: 18px;
+  margin-right: 10px;
+}
+.cinema-item__score {
+  display: inline-block;
+  vertical-align: bottom;
+}
+.cinema-item__price-col, .cinema-item__button-col {
+  height: 88px;
+  line-height: 88px;
+}
+</style>
--- src/pages/cinema-info.vue
+++ src/pages/cinema-info.vue
@@ -1,26 +1,67 @@
 <template>
   <div class="cinema-info">
-    <top-bar></top-bar>
-    <h1>{{ msg }}</h1>
+    <el-breadcrumb class="cinema-info__title">
+      <el-breadcrumb-item to="/">电影</el-breadcrumb-item>
+      <el-breadcrumb-item>{{movieTitle}}</el-breadcrumb-item>
+    </el-breadcrumb>
+    <cinema-item v-for="cinema in cinemas" :cinema="cinema" :key="cinema.name"></cinema-item>
   </div>
 </template>
 
 <script>
-import TopBar from '../components/top-bar'
 
 export default {
   name: 'cinema-info',
   data () {
     return {
-      msg: 'Cinema Info'
+      film: '加勒比海盗5：死无对证',
+      cinemas: [{
+        name: '1978电影城',
+        score: '3.0',
+        location: '增城区增江街沿江东三路15号1978创意园B区1号',
+        times: ['22:25'],
+        price: 33
+      }, {
+        name: 'CGV星聚汇影城(永旺店)',
+        score: '4.8',
+        location: '番禺区亚运大道1号永旺梦乐城4F',
+        times: ['22:50'],
+        price: 37
+      }, {
+        name: 'IDC国际影城(白云尚城店)',
+        score: '5.0',
+        location: '白云区尚景街1号白云尚城文化活动中心',
+        times: ['22:05', '22:35', '23:05'],
+        price: 42
+      }, {
+        name: 'SFC上影影城(海珠燕汇店) ',
+        score: '4.6',
+        location: '海珠区江燕路108号燕汇广场4楼',
+        times: ['21:50', '22:45'],
+        price: 34
+      }]
     }
   },
   components: {
-    'top-bar': TopBar
+    cinemaItem: require('../components/cinema-info/cinema-item.vue')
+  },
+  computed: {
+    movieTitle: function () {
+      let index = this.$route.query.movieIndex
+      return this.$store.state.movieBuff[index].title
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.cinema-info {
+  width: 60%;
+  margin: 0 auto;
+}
+.cinema-info__title {
+  margin-left: 20px;
+  padding: 20px 0;
+  font-size: 24px;
+}
 </style>
