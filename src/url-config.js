f6f85359cc431b55d2797d66859cce7da2d5acc7
hhk <codeforsomeone@gmail.com>
2017-06-02 11:07:25 +0800

    添加测试用api接口

--- src/url-config.js
+++ src/url-config.js
@@ -3,5 +3,6 @@
 
 export default {
   //  Web Server Base URL
-  webServerURL: 'http://139.199.213.19:8080/api/'
+  webServerURL: 'http://139.199.213.19:8080/api/',
+  testUrl: 'https://www.easy-mock.com/mock/5930d36c91470c0ac1ff822a/gomovie/api/'
 }
