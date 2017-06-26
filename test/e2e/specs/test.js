1d8b2ec2df06890b97ec0092c58b2a509630e09c
hhk <codeforsomeone@gmail.com>
2017-06-26 21:33:46 +0800

    尝试添加简单的E2E测试

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -8,12 +8,18 @@ module.exports = {
     // see nightwatch.conf.js
     const devServer = browser.globals.devServerURL
 
-    browser
-      .url(devServer)
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.home')
-      // .assert.containsText('h1', 'Hello Vue, this is home page')
-      // .assert.elementCount('img', 1)
-      .end()
+    browser.url(devServer).pause(1000);
+
+    browser.expect.element('#app').to.be.present
+
+    // 一开始加载，后面加载完了
+    // browser.expect.element('.el-loading-text').to.be.visible
+    browser.expect.element('.el-loading-text').to.be.not.visible.after(5000)
+
+    // 点击第一张海报跳转
+    browser.click('.home-posters__poster').pause(1000);
+    browser.expect.element('.movie-info').to.be.visible
+
+    browser.end()
   }
 }
